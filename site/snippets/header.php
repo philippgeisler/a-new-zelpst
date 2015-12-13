fc18a2ea456998a9b59f236f5828b140d2cd0fd0
Philipp Geisler <philippgeisler@mailbox.org>
2015-12-13 16:18:52 +0100

    octavo.scss incl. reset, fonts

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -5,6 +5,8 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
+  <?php echo css('assets/css/octavo.css') ?>
+
   <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
 
 </head>
