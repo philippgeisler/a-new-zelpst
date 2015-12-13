2d924ef428b4081e995f633cecc036b933c69605
Philipp Geisler <philippgeisler@mailbox.org>
2015-12-13 17:35:00 +0100

    Font Awesome 4.5

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,9 +1,8 @@
 <!-- footer.php -->
 	<footer>
 		<div class="feet">
+            <div class="toe"><a href="<?php echo url('blog/feed') ?>"><span class="fa fa-rss-square fa-lg"></span></a></div>
 			<div class="toe"><a href="<?php echo $pages->find('impressum')->url() ?>">Impressum/Datenschutz</a></div>
-			<div class="toe"><a href="<?php echo url('blog/feed') ?>">RSS</a></div>
-			<div class="toe colophon">zɛlpst is running on <a href="http://getkirby.com">Kirby</a> in <a href="http://uberspace.de">Uberspace</a></div>
 		</div>
 	</footer>
 </body>
